2fd4d97ffa956111ddcd0de9b40c920e038fda35
Yan <yan@yanjobs.me>
2022-10-04 14:55:27 +0200

    feat(bash): user input, better dividers

--- bash/views.sh
+++ bash/views.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-echo "-------------------------------------"
-echo "Getting the url: $1, 100 times"
-echo "-------------------------------------"
-for i in {1..100}
-do
-    echo "Getting the $i views"
-    wget -q "$2"
-    echo "-------------------------------------"
+COUNTER=0
+read -p "How many views do you want to get ? " NUMBER_VIEWS
+read -p "What is the url of the counter ? (GitHub camo works fine): " C_URL
+
+printf "%60s" " " | tr ' ' '-'
+echo "" 
+echo "Farming $NUMBER_VIEWS views on url $C_URL"
+printf "%60s" " " | tr ' ' '-' 
+echo ""
+
+while true; do
+  [[ $COUNTER -eq $NUMBER_VIEWS ]] && echo "Number of views reached, exiting..." && exit
+  HUMAN_COUNTER=$((COUNTER+1))
+  echo "Farming the $HUMAN_COUNTER st/nd/th view..."
+  wget -q "$C_URL"
+  ((COUNTER++))
+  printf "%60s" " " | tr ' ' '-'
+  echo "" 
 done
